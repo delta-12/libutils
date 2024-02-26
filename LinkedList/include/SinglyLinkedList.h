108660f2f0b83090d9556348bbe50d8d78b86204
delta-12 <btesq235@gmail.com>
2024-02-26 03:04:29 +0100

    LinkedList scaffolding

---
+++ LinkedList/include/SinglyLinkedList.h
@@ -0,0 +1,26 @@
+/**
+ * @file SinglyLinkedList.h
+ *
+ * @brief Singly linked list.
+ *
+ ******************************************************************************/
+
+#ifndef SINGLY_LINKED_LIST_H
+#define SINGLY_LINKED_LIST_H
+
+/* Includes
+ ******************************************************************************/
+
+/* Typedefs
+ ******************************************************************************/
+
+typedef struct SinglyLinkedList_Node SinglyLinkedList_Node_t;
+
+struct SinglyLinkedList_Node
+{
+};
+
+/* Function Prototypes
+ ******************************************************************************/
+
+#endif
