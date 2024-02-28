e1503985fa190ef4c2fe8437bb5fca2a50028217
delta-12 <btesq235@gmail.com>
2024-02-28 02:02:18 +0100

    4-linkedlist SinglyLinkedList scaffolding

--- LinkedList/include/SinglyLinkedList.h
+++ LinkedList/include/SinglyLinkedList.h
@@ -11,16 +11,44 @@
 /* Includes
  ******************************************************************************/
 
+#include <stdbool.h>
+#include <stdint.h>
+
 /* Typedefs
  ******************************************************************************/
+
+/* TODO typedef comments */
+
+typedef int64_t SinglyLinkedList_Index_t;
+typedef SinglyLinkedList_Index_t SinglyLinkedList_Length_t;
 
 typedef struct SinglyLinkedList_Node SinglyLinkedList_Node_t;
 
 struct SinglyLinkedList_Node
 {
+    SinglyLinkedList_Node_t *Next;
+    void *Item;
 };
+
+typedef struct
+{
+    SinglyLinkedList_Node_t *Head;
+    SinglyLinkedList_Length_t Length;
+} SinglyLinkedList_t;
 
 /* Function Prototypes
  ******************************************************************************/
+
+bool SinglyLinkedList_Init(SinglyLinkedList_t *const list);
+bool SinglyLinkedList_Free(SinglyLinkedList_t *const list);
+bool SinglyLinkedList_Append(SinglyLinkedList_t *const list, const void *const item);
+bool SinglyLinkedList_AppendLeft(SinglyLinkedList_t *const list, const void *const item);
+bool SinglyLinkedList_Pop(SinglyLinkedList_t *const list, void *const item);
+bool SinglyLinkedList_PopLeft(SinglyLinkedList_t *const list, void *const item);
+bool SinglyLinkedList_Reset(SinglyLinkedList_t *const list, const void *const item);
+SinglyLinkedList_Index_t SinglyLinkedList_GetIndex(const SinglyLinkedList_t *const list);
+SinglyLinkedList_Length_t SinglyLinkedList_GetLength(const SinglyLinkedList_t *const list);
+bool SinglyLinkedList_Insert(SinglyLinkedList_t *const list, const void *const item, const SinglyLinkedList_Index_t index);
+bool SinglyLinkedList_Remove(SinglyLinkedList_t *const list, const SinglyLinkedList_Index_t index);
 
 #endif
--- LinkedList/src/SinglyLinkedList.c
+++ LinkedList/src/SinglyLinkedList.c
@@ -9,3 +9,30 @@
  ******************************************************************************/
 
 #include "SinglyLinkedList.h"
+
+/* Function Definitions
+ ******************************************************************************/
+
+/* TODO comment blocks */
+
+bool SinglyLinkedList_Init(SinglyLinkedList_t *const list) { return false; }
+
+bool SinglyLinkedList_Free(SinglyLinkedList_t *const list) { return false; }
+
+bool SinglyLinkedList_Append(SinglyLinkedList_t *const list, const void *const item) { return false; }
+
+bool SinglyLinkedList_AppendLeft(SinglyLinkedList_t *const list, const void *const item) { return false; }
+
+bool SinglyLinkedList_Pop(SinglyLinkedList_t *const list, void *const item) { return false; }
+
+bool SinglyLinkedList_PopLeft(SinglyLinkedList_t *const list, void *const item) { return false; }
+
+bool SinglyLinkedList_Reset(SinglyLinkedList_t *const list, const void *const item) { return false; }
+
+SinglyLinkedList_Index_t SinglyLinkedList_GetIndex(const SinglyLinkedList_t *const list) { return 0L; }
+
+SinglyLinkedList_Length_t SinglyLinkedList_GetLength(const SinglyLinkedList_t *const list) { return 0L; }
+
+bool SinglyLinkedList_Insert(SinglyLinkedList_t *const list, const void *const item, const SinglyLinkedList_Index_t index) { return false; }
+
+bool SinglyLinkedList_Remove(SinglyLinkedList_t *const list, const SinglyLinkedList_Index_t index) { return false; }
