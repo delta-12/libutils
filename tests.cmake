e19e82e3f976bdd5cc82224cb0ec970ecc4907a2
delta-12 <btesq235@gmail.com>
2024-03-11 03:06:41 +0100

    3-branch-protection-tests-and-actions add tests with valgrind to cmake

---
+++ tests.cmake
@@ -0,0 +1,17 @@
+include(CTest)
+enable_testing()
+
+function(add_test_with_valgrind name)
+  add_test(NAME ${name} COMMAND ${name})
+  add_test(NAME ${name}_valgrind
+    COMMAND valgrind
+      --tool=memcheck
+      --leak-check=full       
+      --trace-children=yes
+      --show-error-list=yes
+      --error-exitcode=2
+      --xml=yes
+      --xml-file=${name}-valgrind-report.xml
+      $<TARGET_FILE:${name}>
+)
+endfunction()
