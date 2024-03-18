c24455333a0b4ed079a0b85b647bd7f3a4345722
delta-12 <btesq235@gmail.com>
2024-03-18 16:24:51 +0100

    18-debug-release-builds re-organize project and consolidate cmake

---
+++ include/Version.h
@@ -0,0 +1,18 @@
+/**
+ * @file Version.h
+ *
+ * @brief Project version numbers derived from CMake.
+ *
+ ******************************************************************************/
+
+#ifndef VERSION_H
+#define VERSION_H
+
+/* Defines
+ ******************************************************************************/
+
+#define VERSION_MAJOR @libutils_VERSION_MAJOR@
+#define VERSION_MINOR @libutils_VERSION_MINOR@
+#define VERSION_PATCH @libutils_VERSION_PATCH@
+
+#endif
