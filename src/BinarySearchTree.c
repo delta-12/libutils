55fc4ab82e27eb4a3283ae5436f5bc2e83f9330a
delta-12 <btesq235@gmail.com>
2024-05-02 18:07:18 +0200

    21-binary-search-tree document bst methods

--- src/BinarySearchTree.c
+++ src/BinarySearchTree.c
@@ -24,6 +24,17 @@ static void BinarySearchTree_GetSuccessor(BinarySearchTree_Node_t *const node, B
 /* Function Definitions
  ******************************************************************************/
 
+/**
+ * @brief Initialize a binary search tree.
+ *
+ * @param[in,out] tree     Pointer to the tree to initialize
+ * @param[in]     itemSize Size in bytes of the items held in the tree
+ *
+ * @return Whether the tree was successfully initialized or not
+ *
+ * @retval true  Successfully initialized
+ * @retval false Failed to initialize
+ ******************************************************************************/
 bool BinarySearchTree_Init(BinarySearchTree_t *const tree, const size_t itemSize)
 {
   bool init = false;
@@ -39,11 +50,33 @@ bool BinarySearchTree_Init(BinarySearchTree_t *const tree, const size_t itemSize
   return init;
 }
 
+/**
+ * @brief Free a binary search tree.
+ *
+ * @param[in,out] tree Pointer to the tree to free
+ *
+ * @note This function merely calls BinarySearchTree_Reset.  See the
+ *       documentation for BinarySearchTree_Reset for more information.
+ ******************************************************************************/
 bool BinarySearchTree_Free(BinarySearchTree_t *const tree)
 {
   return BinarySearchTree_Reset(tree);
 }
 
+/**
+ * @brief Insert an item into a binary search tree.  Items are stored by copy,
+ *        not reference.
+ *
+ * @param[in,out] tree Pointer to the tree to insert the item into
+ * @param[in]     key  Key for the item to be inserted into the tree, used to
+ *                     maintain the order of the tree
+ * @param[in]     item Pointer to the item to insert into the tree
+ *
+ * @return Whether the item was successfully inserted into the tree or not
+ *
+ * @retval true  Successfully inserted the item into the tree
+ * @retval false Failed to insert the item into the tree
+ ******************************************************************************/
 bool BinarySearchTree_Insert(BinarySearchTree_t *const tree, const BinarySearchTree_Key_t key, const void *const item)
 {
   bool inserted = false;
@@ -90,6 +123,17 @@ bool BinarySearchTree_Insert(BinarySearchTree_t *const tree, const BinarySearchT
   return inserted;
 }
 
+/**
+ * @brief Remove an item from a binary search tree.
+ *
+ * @param[in,out] tree Pointer to the tree to remove the item from
+ * @param[in]     key  Key of the item to remove from the tree
+ *
+ * @return Whether the item was successfully removed from the tree or not
+ *
+ * @retval true  Successfully removed the item from the tree
+ * @retval false Failed to remove the item from the tree
+ ******************************************************************************/
 bool BinarySearchTree_Remove(BinarySearchTree_t *const tree, const BinarySearchTree_Key_t key)
 {
   bool removed = false;
@@ -147,6 +191,18 @@ bool BinarySearchTree_Remove(BinarySearchTree_t *const tree, const BinarySearchT
   return removed;
 }
 
+/**
+ * @brief Search for an item in a binary search tree.
+ *
+ * @param[in,out] tree Pointer to the tree to search for the item in
+ * @param[in]     key  Key of the item to search for in the tree
+ * @param[out]    item Pointer to a buffer large enough to copy the item into
+ *
+ * @return Whether the item was successfully found in the tree or not
+ *
+ * @retval true  Successfully found the item in the tree
+ * @retval false Failed to find the item in the tree
+ ******************************************************************************/
 bool BinarySearchTree_Search(const BinarySearchTree_t *const tree, const BinarySearchTree_Key_t key, void *const item)
 {
   bool found = false;
@@ -169,6 +225,11 @@ bool BinarySearchTree_Search(const BinarySearchTree_t *const tree, const BinaryS
   return found;
 }
 
+/**
+ * @brief Reset a binary search tree to its initial state.
+ *
+ * @param[in,out] tree Pointer to the tree to reset
+ ******************************************************************************/
 bool BinarySearchTree_Reset(BinarySearchTree_t *const tree)
 {
   bool reset = false;
@@ -203,6 +264,17 @@ bool BinarySearchTree_Reset(BinarySearchTree_t *const tree)
   return reset;
 }
 
+/**
+ * @brief Get a pointer to a node and its parent in a binary search tree.
+ *
+ * @param[in]  tree   Pointer to the tree to get the node from
+ * @param[in]  key    Key of the node to get from the tree
+ * @param[out] node   Pointer to the node from the tree, NULL if there is not a
+ *                    node with the key in the tree
+ * @param[out] parent Pointer to the parent of the node from the tree, NULL if
+ *                    there is not a node with the key in the tree or the node
+ *                    with the key does not have a parent, i.e. it is the root
+ ******************************************************************************/
 static void BinarySearchTree_GetNode(const BinarySearchTree_t *const tree,
                                      const BinarySearchTree_Key_t key,
                                      BinarySearchTree_Node_t **const node,
@@ -226,6 +298,17 @@ static void BinarySearchTree_GetNode(const BinarySearchTree_t *const tree,
   }
 }
 
+/**
+ * @brief Get a pointer to a node's inorder successor and the successor's
+ *        parent in a binary search tree.
+ *
+ * @param[in]  node      Pointer to the node to get the successor of
+ * @param[out] successor Pointer to the successor from the tree, NULL if there
+ *                       is not a successor in the tree
+ * @param[out] parent    Pointer to the parent of the successor from the tree,
+ *                       points to the node if there is not a successor in the
+ *                       tree
+ ******************************************************************************/
 static void BinarySearchTree_GetSuccessor(BinarySearchTree_Node_t *const node, BinarySearchTree_Node_t **const successor, BinarySearchTree_Node_t **const parent)
 {
   *successor = NULL;
